7bfa8f46992cb81a1e81eb6f3304f633801f9016
chickenbestlover <jmpark@rit.kaist.ac.kr>
2018-05-31 20:32:07 +0900

    fix cpu mode bug

--- interact_langs_ELMo.py
+++ interact_langs_ELMo.py
@@ -32,7 +32,7 @@ class SampleGen:
 
         options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
         weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
-        self.elmo = Elmo(options_file, weight_file, 2, dropout=0).to('cuda')
+        self.elmo = Elmo(options_file, weight_file, 2, dropout=0).to('cuda' if self.gpu else 'cpu')
 
     def process(self,batch):
             batch_size = len(batch)
